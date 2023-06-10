adb65864cb37b11e31f133b1aca0394ff2276a70
lu <2469480096@qq.com>
2023-06-10 17:31:23 +0800

    add base ui

--- app/src/main/java/com/bedroom412/ylgy/MainActivity.kt
+++ app/src/main/java/com/bedroom412/ylgy/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.bedroom412.ylgy
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.bedroom412.ylgy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initStatusBar()
+
+        binding.settingsBtn.setOnClickListener {  }
+        binding.scanBtn.setOnClickListener {  }
+        binding.searchBtn.setOnClickListener {  }
+        // 打开播放器
+        binding.vinylRecordView.setOnClickListener {  }
+        binding.songCover.setOnClickListener {  }
+    }
+
+    private fun initStatusBar() {
+        binding.songTitle.isSelected = true
+        window.statusBarColor = Color.TRANSPARENT
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
