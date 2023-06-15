a779187312305a84d796bb98729fc7adb07bcbad
zxw7606 <zxw7606@outlook.com>
2023-06-15 19:01:59 +0800

    feat: play activity

---
+++ app/src/main/java/com/bedroom412/ylgy/activity/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.bedroom412.ylgy.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bedroom412.ylgy.R
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
